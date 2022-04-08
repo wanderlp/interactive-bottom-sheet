b47dfc150541d4cd362f8333417730b5591ee6b2
Wanderson <lopez.wanderson@gmail.com>
2022-04-08 14:28:05 -0600

    Creación del proyecto

---
+++ InteractiveBottomSheet/InteractiveBottomSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InteractiveBottomSheet
+//
+//  Created by Wanderson López on 8/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InteractiveBottomSheet/InteractiveBottomSheet/InteractiveBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveBottomSheetApp.swift
+//  InteractiveBottomSheet
+//
+//  Created by Wanderson López on 8/04/22.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
