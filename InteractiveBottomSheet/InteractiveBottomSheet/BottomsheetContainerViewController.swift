2645ef14b0774b3280a48d5d1c061d04d1a5338d
Wanderson <lopez.wanderson@gmail.com>
2022-04-08 14:34:14 -0600

    Se creó el inicializador

--- InteractiveBottomSheet/InteractiveBottomSheet/BottomsheetContainerViewController.swift
+++ InteractiveBottomSheet/InteractiveBottomSheet/BottomsheetContainerViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 open class BottomSheetContainerViewController<Content: UIViewController, BottomSheet: UIViewController>: UIViewController {
+    
+    // MARK: - Initialization
+    public init(contentViewController: Content, bottomSheetViewController: BottomSheet) {
+        self.contentViewController = contentViewController
+        self.bottomSheetViewController = bottomSheetViewController
+        
+        super.init(nibName: nil, bundle: nil)
+    }
 
     // MARK: - Children
     let contentViewController: Content
