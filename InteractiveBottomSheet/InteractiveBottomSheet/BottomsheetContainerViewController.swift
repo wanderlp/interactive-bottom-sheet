1a4641454ce64b7ec178a54d2f1f8d4c2be0ee06
Wanderson <lopez.wanderson@gmail.com>
2022-04-08 17:37:52 -0600

    Se implementó la función handlePan

--- InteractiveBottomSheet/InteractiveBottomSheet/BottomsheetContainerViewController.swift
+++ InteractiveBottomSheet/InteractiveBottomSheet/BottomsheetContainerViewController.swift
@@ -53,8 +53,6 @@ open class BottomSheetContainerViewController<Content: UIViewController, BottomS
     
     
     // MARK: - Properties that handle interaction and animation.
-    @objc func handlePan(_ sender: UIPanGestureRecognizer) {}
-    
     lazy var panGesture: UIPanGestureRecognizer = {
         let pan = UIPanGestureRecognizer()
         pan.delegate = self
@@ -163,6 +161,114 @@ open class BottomSheetContainerViewController<Content: UIViewController, BottomS
         } else {
             self.view.layoutIfNeeded()
             self.state = .initial
+        }
+    }
+    
+    // Works directly with the UIPanGestureRecognizer.
+    // Allows move the bottom sheet with our finger up and down.
+    // When we stop moving it, take into account the current BottomSheetState,
+    // translation, and velocity in the y direction. If the state is .full, check if
+    // we have the botom sheet translation of at least half of its height. If this is
+    // true, we run the hideBottomSheet(animated:) method. Otherwise, we return
+    // it to the full height. Also, we need to chek if the magnitude of the velocity
+    // is greater than 1,000. If this give us true, hide the bottom sheet. In other
+    // cases, revert it to the full height. Similarly, if the state is .initial, check
+    // the translation and velocity mangnitudes and react accordingly.
+    @objc func handlePan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: bottomSheetViewController.view)
+        let velocity = sender.velocity(in: bottomSheetViewController.view)
+        
+        let yTranslationMagnitude = translation.y.magnitude
+        
+        switch sender.state {
+            case .began, .changed:
+                if self.state == .full {
+                    // 1 - Assert that the user scroll downward. The bottom
+                    //     sheet is already at its full height; no upward
+                    //     scrolling is allowed
+                    guard translation.y > 0 else { return }
+                    
+                    // 2 - Change the topConstraint's constant to match the
+                    //     current position of the user's finger. For example,
+                    //     if the total height of the bottom sheet is 500 points
+                    //     and the user has scrolled 100 points, we substract 100
+                    //     from 5000 and obtain 400 points. So we set the constant
+                    //     to -400, because this is an offset from the bottom of
+                    //     the contaiiner's view
+                    topConstraint.constant = -(configuration.height - yTranslationMagnitude)
+                    
+                    // 3 - Update the root view to show the constraint's change
+                    self.view.layoutIfNeeded()
+                } else {
+                    // 4 - Calculate the new constant by using the BottomSheetConfiguration's
+                    //     initialOffset and the translation magnitude. For example,
+                    //     if the initial offset was 80 points and the user has scrolled
+                    //     200 points, we obtain 280 points. This means we would need
+                    //     to place the bottom sheet 280 points from the bottom of the
+                    //     container's view
+                    let newConstant = -(configutarion.initialOffset + yTranslationMagnitude)
+                    
+                    // 5 - Assert that the user scrolls upward. Because the bottom
+                    //     sheet is at its initial point, no downward scrolling is
+                    //     allowed
+                    guard translation.y < 0 else { return }
+                    
+                    // 6 - Assert that the magnitude of the newConstant is less than
+                    //     the full height of the bottom sheet. This is to prevent
+                    //     the bottom sheet from moving further than its maximum
+                    //     height point
+                    guard newConstant.magnitude < configuration.height else {
+                        self.showBottomSheet()
+                        return
+                    }
+                    
+                    // 7 - Set the resulting constant
+                    topConstraint.constant = newConstant
+                    
+                    // 8 - Update the root view to show the constraint's change
+                    self.view.layoutIfNeeded()
+                }
+            case .ended {
+                if self.state == .full {
+                    // 1 - Check if the user has tried to move the bottom sheet
+                    //     upwards. If this is true, bottom sheet should remain
+                    //     shown
+                    if velocity.y < 0 {
+                        selft.showBottomSheet()
+                    } else if yTranslationMagnitude >= configuration.height / 2 || velocity.y > 1000 {
+                        // 2 - If the user moved the bottom sheet more than half
+                        //     or its maximum height, or the y velocity is higher
+                        //     than 1,000, then we hide the bottom sheet
+                        self.hideBottomSheet()
+                    }
+                    else {
+                        // 3 - In all other cases, keep the sheet shown
+                        self.showBottomSheet()
+                    }
+                }
+                else {
+                    // 4 - Check if the user has moved the bottom sheet at least
+                    //     half of its maximum height, or the y velocity is less
+                    //     than -1,000
+                    if yTranslationMagnitude >= configuration.height / 2 || velocity.y < -1000 {
+                        // 5 - If is true, show the bottom sheet
+                        self.showBottomSheet()
+                    }
+                    else {
+                        // 6 - Otherwise, hide the bottom sheet
+                        self.hideBottomSheet()
+                    }
+                }
+            }
+            case .failed {
+                if self.stat == .full {
+                    self.showBottomSheet()
+                }
+                else {
+                    self.hideBottomSheet()
+                }
+            }
+            default: break
         }
     }
 }
