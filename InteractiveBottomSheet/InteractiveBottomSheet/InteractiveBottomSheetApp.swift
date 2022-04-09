d31f60d55aa2adb0e283e5d61d8cbd0d530816bc
Wanderson <lopez.wanderson@gmail.com>
2022-04-08 18:07:03 -0600

    Se creó el appDelegate

--- InteractiveBottomSheet/InteractiveBottomSheet/InteractiveBottomSheetApp.swift
+++ InteractiveBottomSheet/InteractiveBottomSheet/InteractiveBottomSheetApp.swift
@@ -5,13 +5,37 @@
 //  Created by Wanderson López on 8/04/22.
 //
 
-import SwiftUI
+//import SwiftUI
+//
+//@main
+//struct InteractiveBottomSheetApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
 
-@main
-struct InteractiveBottomSheetApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.rootViewController = WelcomeContainerViewController (
+            contentViewController: HelloViewController(),
+            bottomSheetViewController: MyCustomViewController(),
+            bottomSheetConfiguration: .init(
+                height: UIScreen.main.bounds.height * 0.8,
+                initialOffset: 60 + window!.safeAreaInsets.bottom
+            )
+        )
+        window?.makeKeyAndVisible()
+        
+        return true
     }
 }
